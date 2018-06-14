fa1592ecfcceb70fdd65a69c833a280f37ac80b2
Георгий Санадзе <goshik199956@gmail.com>
2018-06-14 22:07:47 +0300

    Add UI

---
+++ MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using System;
+using Android.Content;
+using Android.Graphics;
+
+namespace TicTacToe
+{
+    [Activity(Label = "TICTAETOE", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Android.App.Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+        }
+    }
+}
+
---
+++ MainScreenActivity.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Graphics;
+using Java.Lang;
+using Android.Views.Animations;
+using Android.Animation;
+using Android.Util;
+
+namespace TicTacToe
+{
+    [Activity(Label = "MainScreen")]
+    public class MainScreenActivity : Android.App.Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+        }      
+    }
+}
